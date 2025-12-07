8c4f40bd3a26542db3b996bec4d5f346a6fe9fb5
GAMaksim <2313123m@jdu.uz>
2025-12-08 01:10:35 +0500

    fix bug and change image location

--- backend/app/services/image_service.py
+++ backend/app/services/image_service.py
@@ -2,7 +2,7 @@ import json
 import os
 import re
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), ". .", "data")
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
 
 
 class ImageService:
